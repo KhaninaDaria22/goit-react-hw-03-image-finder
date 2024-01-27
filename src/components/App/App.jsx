3684e2fb801f7def51d4f7f33fcc359a3e0e2234
Daria Khanina <khanina_dasha@ukr.net>
2024-01-28 00:12:30 +0200

    commit 1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
