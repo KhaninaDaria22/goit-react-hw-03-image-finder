6cb44dde4468b7f9bcc92aa439ae9539668fe382
Daria Khanina <khanina_dasha@ukr.net>
2024-02-03 23:33:43 +0200

    commit 12

--- src/services/img-api.jsx
+++ src/services/img-api.jsx
@@ -17,7 +17,7 @@ export const searchImage = ( q, page=1) => {
             key: "40998806-b427a66e0a96d7aa9b4c9074f",
             image_type: "photo",
             orientation: "horizontal",
-            pere_page: 12,
+            per_page: 12,
         }
     })
 }
